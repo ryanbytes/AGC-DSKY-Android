94c08edaa54d77a94a7450bb8aca3a51181e1fe4
ryanbytes <45298254+ryanbytes@users.noreply.github.com>
2026-09-08 13:19:33 -0400

    Use modern system bar APIs in dream service

--- app/src/main/java/org/apollo/agcdsky/AgcDreamService.java
+++ app/src/main/java/org/apollo/agcdsky/AgcDreamService.java
@@ -1,8 +1,12 @@
 package org.apollo.agcdsky;
 
 import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.service.dreams.DreamService;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.JavascriptInterface;
@@ -28,13 +32,7 @@ public final class AgcDreamService extends DreamService {
         setScreenBright(true);
 
         setWindowBrightness(0.06f);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        hideSystemBars();
 
         if ((getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
             WebView.setWebContentsDebuggingEnabled(true);
@@ -76,6 +74,37 @@ public final class AgcDreamService extends DreamService {
                     "DreamService startup failure\n" + error.toString());
             finish();
         }
+    }
+
+    private void hideSystemBars() {
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            hideModernSystemBars(window);
+        } else {
+            hideLegacySystemBars(window.getDecorView());
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private static void hideModernSystemBars(Window window) {
+        window.setDecorFitsSystemWindows(false);
+        WindowInsetsController controller = window.getInsetsController();
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.systemBars());
+            controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private static void hideLegacySystemBars(View decor) {
+        decor.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
     private void setWindowBrightness(float value) {
